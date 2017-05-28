f9e06d547e4c78fded93bed1260223f1f7f3229d
Brian <sbardian@gmail.com>
2017-05-28 11:33:28 -0600

    Reading posts of type slider to build sliders.

--- public/slider.php
+++ public/slider.php
@@ -13,6 +13,58 @@
 ?>
 
 <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <!-- Indicators -->
+                <?php
+                $index1 = 0;
+                $args = array('post_type' => 'sliders', 'posts_per_page' => 10 );
+                $the_query = new WP_Query($args);
+                if ( $the_query->have_posts() ) :
+                    while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <li data-target="#carousel" data-slide-to="<?php echo $index1; ?>" class="<?php echo ($index === 0) ? 'active' : '' ?>"></li>
+                       <?php $index1++; ?>
+                    <?php endwhile; ?>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <?php $index2 = 0; ?>
+                    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+                        <div class="item <?php echo ($index2 === 0) ? ' active' : '' ?>">
+                            <img src="<?php echo plugins_url('/images/default-slider.gif', __FILE__); ?>" alt="image1" style="width:100%;">
+                            <div class="carousel-caption">
+                                <?php the_title(); ?>
+                                <br/>
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                      <?php $index2++; ?>
+                  <?php endwhile; ?>
+                  <?php else : ?>
+                      <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                  <?php endif; ?>
+                </div>
+
+                <!-- Controls -->
+                <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<hr style="width: 100%;"/>
+
+<div class="container">
   <div class="row">
     <div class="col-md-12">
       <div id="carousel" class="carousel slide" data-ride="carousel">
