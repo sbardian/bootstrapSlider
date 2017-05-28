f56101a5f5ba6253baacf93d9e7d7ff4e2d48dae
Brian <sbardian@gmail.com>
2017-05-28 11:52:15 -0600

    Add using featured image as slider image.

--- public/slider.php
+++ public/slider.php
@@ -33,7 +33,12 @@
                     <?php $index2 = 0; ?>
                     <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
                         <div class="item <?php echo ($index2 === 0) ? ' active' : '' ?>">
-                            <img src="<?php echo plugins_url('/images/default-slider.gif', __FILE__); ?>" alt="image1" style="width:100%;">
+                            <?php
+                                $thumb_id = get_post_thumbnail_id();
+                                $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'full', true);
+                                $thumb_url = $thumb_url_array[0];
+                            ?>
+                            <img src="<?php echo $thumb_url ?>" alt="image1" style="width:100%;">
                             <div class="carousel-caption">
                                 <?php the_title(); ?>
                                 <br/>
@@ -61,45 +66,3 @@
     </div>
 </div>
 
-
-<hr style="width: 100%;"/>
-
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <div id="carousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-          <li data-target="#carousel" data-slide-to="0" class="active"></li>
-          <li data-target="#carousel" data-slide-to="1"></li>
-        </ol>
-
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
-          <div class="item active">
-            <img src="<?php echo plugins_url('/images/default-slider.gif', __FILE__); ?>" alt="image1" style="width:100%;">
-            <div class="carousel-caption">
-              slider 1
-            </div>
-          </div>
-          <div class="item">
-            <img src="<?php echo plugins_url('/images/default-slider.gif', __FILE__); ?>" alt="image2" style="width:100%;">
-            <div class="carousel-caption">
-              slider 2
-            </div>
-          </div>
-        </div>
-
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
